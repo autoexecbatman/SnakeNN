201bd34dbe29ece45d81b07a0f5c3f4297e0d8c8
autoexecbatman <krukstanislav@gmail.com>
2025-07-15 21:56:22 +0300

    Remove temporary setup scripts

--- quick_setup.bat
+++
@@ -1,7 +0,0 @@
-cd D:\repo\snakeNN
-git init
-git remote add origin https://github.com/autoexecbatman/SnakeNN.git
-git add .
-git commit -m "Initial commit: Complete SnakeNN project with DQN implementation"
-git branch -M main
-git push -u origin main
